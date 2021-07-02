96a70333e5bb6275ca6840527c9758ec9ccd9bb5
joshhunt1991 <Joshhunt_1991@hotmail.co.uk>
2021-07-02 13:44:38 +0000

    changed nav bar colour and added a logo font

--- mindawake/settings.py
+++ mindawake/settings.py
@@ -44,6 +44,11 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+
+    # Other
+    'crispy_forms',
+    'storages',
+
 ]
 
 MIDDLEWARE = [
@@ -57,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'mindawake.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -74,6 +81,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
