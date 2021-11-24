eda057d333de23ff949695eecea6baf4847f54d7
joshhunt1991 <Joshhunt_1991@hotmail.co.uk>
2021-11-24 19:58:33 +0000

    added functionality for uploading images of tattoos and their category and description

--- gallery/views.py
+++ gallery/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
+from .models import Category, Tattoo
 
 # Create your views here.
 
 
-def gallery(request):
+def gallery(request):    
     """ renders the index page template """
+    categories = Category.objects.all()
+    tattoos = Tattoo.objects.all()
+    context = {'categories':categories, 'tattoos':tattoos}
 
-    return render(request, 'gallery/gallery.html')
+    return render(request, 'gallery/gallery.html', context)
 
 
 def viewTattoo(request, pk):
