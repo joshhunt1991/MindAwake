fe92779bf84ae7512cdc132062bac0dd3c54a75a
joshhunt1991 <Joshhunt_1991@hotmail.co.uk>
2022-03-12 12:55:53 +0000

    updated category nav to be full width of screen on tablets and mobiles

--- mindawake/settings.py
+++ mindawake/settings.py
@@ -169,6 +169,15 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+AWS_QUERYSTRING_AUTH = False
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = 'AKIAXDBJYG5GU2TM2C6N'
+AWS_SECRET_ACCESS_KEY = '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'
+
+AWS_STORAGE_BUCKET_NAME = 'mind-awake'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
